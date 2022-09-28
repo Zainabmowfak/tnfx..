2204f800a6541c6c1aaa2f750b36b72d40f5aa79
Zainab Mowfak <103317973+Zainabmowfak@users.noreply.github.com>
2022-09-27 23:28:48 -0700

    Add files via upload

---
+++ js/platorm.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector('.menu__wrapper');
+
+wrapper.addEventListener('click', () => {
+  wrapper.classList.toggle('active');
+})
+const wrapper1 = document.querySelector('.menu__wrapper1');
+
+wrapper1.addEventListener('click', () => {
+wrapper1.classList.toggle('active');
+});
